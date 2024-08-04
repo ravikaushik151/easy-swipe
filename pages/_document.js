5788cf50424bba6887e48b23ec7b540ccb22d490
ravikaushik151 <ravi.k@imsolutions.mobi>
2024-08-04 23:07:49 +0530

    commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,18 @@
-import { useEffect } from 'react';
+import { Html, Head, Main, NextScript } from 'next/document';
 
-function MyComponent() {
-  useEffect(() => {
-    // Create a script element
-    const script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-    script.async = true;
-    // Append the script to the document body
-    document.body.appendChild(script);
-
-    // Clean up the script when the component unmounts
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
-
-  return <div>Content of the component</div>;
+export default function Document() {
+  return (
+    <Html>
+      <Head>        
+        {/* Link to the favicon */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* Add any other head elements here */}
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>
+      </body>
+    </Html>
+  );
 }
-
-export default MyComponent;
