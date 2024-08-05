95a2f9c29d778e93025dea1487eeb2fa2d9f7fe9
Ravi <ravi.k@imsolutions.mobi>
2024-08-05 13:01:11 +0530

    commit

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,114 @@
 // pages/about.js
+import Image from 'next/image';
+
 export default function About() {
     return (
-        <div>
-            <h1>About Page</h1>
-            <p>Learn more about us on this page.</p>
-        </div>
+        <>
+            {/* About breadcrumb area start */}
+            <section className="custom-header" style={{ background: "url('/img/About-Us-header.jpg')" }}>
+            </section>
+            {/* About breadcrumb area end */}
+
+            <section className="mb-4" style={{ marginTop: '-120px' }}>
+                <div className="container">
+                    <div className="row d-flex align-content-center justify-content-center">
+                        <div className="title text-center">
+                            <h2 className="text-main">About Easy Swipe</h2>
+                        </div>
+                        <div className="row d-flex align-items-center text-justify my-3">
+                            <div className="col-lg-6 col-12" data-aos="fade-right">
+                                <Image src="/img/card.jpg" className="img-fluid mb-3" alt="Card" width={500} height={300} />
+                            </div>
+                            <div className="col-lg-6 col-12" data-aos="fade-left">
+                                <p className="text-dark fs-3 fw-bold text-main">
+                                    Your Reliable Source for Quick Cash Against Credit Cards in Bangalore
+                                </p>
+                                <h5 className="fs-5 fw-bold text-main">Instant Money Transfer Services with Easy Swipe</h5>
+                                <p className="lh-lg">
+                                    In today’s fast-paced world, managing finances has never been easier thanks to online money transfer services. The traditional days of lengthy bank visits and cumbersome paperwork are now behind us. With Easy Swipe, you can experience the convenience and security of instant online money transfer services.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="section same-gap testimonal" style={{ backgroundColor: '#F6EBE7' }}>
+                <div className="container">
+                    <div className="row py-4">
+                        <div className="title text-center">
+                            <h2 className="text-main">Discover the Benefits of Our Services:</h2>
+                        </div>
+                    </div>
+                    <div className="wow slideInDown">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="mv px-md-5 py-3 px-3 mb-3" style={{ backgroundColor: '#fff' }}>
+                                    <Image src="/img/hand-gesture.png" className="d-block mx-auto img-fluid p-4" alt="Quick Cash Access" width={120} height={120} />
+                                    <h2 className="fs-4 fw-bold text-main text-center">Quick Cash Access</h2>
+                                    <p className="lh-lg text-main" style={{ minHeight: '128px' }}>
+                                        Get spot cash against your credit card instantly, whether you're in Indiranagar or anywhere else in Bangalore. Our credit card swipe for cash service ensures that you receive immediate funds with minimal hassle.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="mv px-md-5 py-3 px-3 mb-3" style={{ backgroundColor: '#fff' }}>
+                                    <Image src="/img/theft.png" className="d-block mx-auto img-fluid p-4" alt="Convenient and Secure Transactions" width={120} height={120} />
+                                    <h2 className="fs-4 fw-bold text-main text-center">Convenient and Secure Transactions</h2>
+                                    <p className="lh-lg text-main" style={{ minHeight: '128px' }}>
+                                        Experience seamless cash withdrawal from your credit card with our secure process. From cash on credit card swipe in Bangalore to efficient cash against credit card services, we offer a fast and reliable way to access the cash you need.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className="section same-gap testimonal" style={{ backgroundColor: '#fff' }}>
+                <div className="container">
+                    <div className="row py-4">
+                        <div className="title text-center">
+                            <h2 className="text-main mb-0">Effortless Financial Transactions</h2>
+                            <p className="text-center">
+                                With Easy Swipe, sending and receiving money has become faster, more secure, and incredibly convenient. Our services have redefined how you manage your finances, providing you with a smooth experience for cash on credit card swipe in Bangalore.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="wow slideInDown">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="mv px-md-5 py-3 px-3">
+                                    <Image src="/img/Quick-Cash-Access.jpg" className="d-block mx-auto img-fluid mb-3" alt="Quick Cash Access" width={500} height={300} />
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="mv px-md-5 py-3 px-3">
+                                    <h3 className="text-dark text-left text-main fs-3 fw-bold">Quick Cash Access</h3>
+                                    <p className="text-justify lh-lg">
+                                        At Easy Swipe, we understand that urgent financial needs require prompt solutions. Our service provides spot cash against your credit card almost instantly. Whether you’re located in Indiranagar or any part of Bangalore, you can rely on us for quick cash access. Our credit card swipe for cash service allows you to convert your credit card limit into immediate funds, ensuring you have the cash you need without unnecessary delays. With our straightforward process, getting cash from your credit card has never been easier.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div className="mv px-md-5 py-3 px-3">
+                                    <h3 className="text-dark text-left text-main fs-3 fw-bold">Convenient and Secure Transactions</h3>
+                                    <p className="text-justify lh-lg">
+                                        Our priority is to offer you a hassle-free and secure experience when you need cash against your credit card. We ensure that every transaction, from cash on credit card swipe in Bangalore to cash withdrawal from your credit card, is processed smoothly and securely. Our system is designed to protect your financial information while providing a swift solution to your cash needs. Whether you require cash on credit card swipe or a quick credit card to cash transfer, our services are tailored to meet your financial requirements with utmost convenience and safety.
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <div className="mv px-md-5 py-3 px-3">
+                                    <Image src="/img/Convenient-and-Secure-Transactions.jpg" className="d-block mx-auto img-fluid mb-3" alt="Convenient and Secure Transactions" width={500} height={300} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 }
